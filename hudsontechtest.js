7e77067089dcf71956690146efe71dac78dd20e7
onitwin <onitwin@gmail.com>
2021-03-26 16:36:12 +0000

    finished base program

--- hudsontechtest.js
+++ hudsontechtest.js
@@ -1,4 +1,5 @@
 const puppeteer=require('puppeteer');
+const jsonfile = require("jsonfile");
 
 async function obtainData(url){
   const browser=await puppeteer.launch();
@@ -8,17 +9,20 @@ async function obtainData(url){
   const list=await page.evaluate(()=>{
     const data=[];
 
-    const list =document.querySelectorAll(".product-tile")
+    const list =document.querySelectorAll(".product-tile") //get all divs class name of ".product-tile" and attaches array to 'list' class
 
       for (const item of list){
-        data.push({"product":item.querySelector(".product-name").innerHTML,"metadata":{"image_url":item.querySelector("img").src}})
+        data.push({"product":item.querySelector(".product-name").innerHTML,"metadata":{"image_url":item.querySelector("img").src,
+      "quantity":parseInt(item.querySelector(".details p").innerHTML.slice(10)),"price":parseInt(item.querySelector('p:nth-child(2)').innerHTML.slice(8))}})
       }
       return data;
     })
 
   console.log(list);
 
+
   browser.close()
+  jsonfile.writeFile("data.json", list)
 }
 
 obtainData('https://dev-test.hudsonstaging.co.uk/')
