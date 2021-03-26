f1fe52058d900fefaa9aa52567676a73f76e5f24
onitwin <onitwin@gmail.com>
2021-03-26 20:13:39 +0000

    added beautify to json and renamed variables for better legibility

--- hudsontechtest.js
+++ hudsontechtest.js
@@ -1,28 +1,30 @@
-const puppeteer=require('puppeteer');
+const puppeteer = require('puppeteer');
 const jsonfile = require("jsonfile");
 
-async function obtainData(url){
-  const browser=await puppeteer.launch();
-  const page=await browser.newPage();
-  await page.goto(url);
+async function obtainData(url) {
+  const browser = await puppeteer.launch();//launch browser
+  const page = await browser.newPage();//get new page within browser
+  await page.goto(url);//load url on new page
 
-  const list=await page.evaluate(()=>{
-    const data=[];
+  const list = await page.evaluate(() => {
+    const data = []; //empty array for inputting formatted data
 
-    const list =document.querySelectorAll(".product-tile") //get all divs class name of ".product-tile" and attaches array to 'list' class
+    const stock = document.querySelectorAll(".product-tile") //get all divs class name of ".product-tile" and attaches array to 'list' class
 
-      for (const item of list){
-        data.push({"product":item.querySelector(".product-name").innerHTML,"metadata":{"image_url":item.querySelector("img").src,
-      "quantity":parseInt(item.querySelector(".details p").innerHTML.slice(10)),"price":parseFloat(item.querySelector('p:nth-child(2)').innerHTML.slice(8))}})
-      }
-      return data;
-    })
-
-  console.log(list);
-
-
-  browser.close()
-  jsonfile.writeFile("data.json", list)
+    for (const item of stock) { //check each item matching 'product-tile' and loop through for requested data and return object
+      data.push({
+        "product": item.querySelector(".product-name").innerHTML,
+        "metadata": {
+          "image_url": item.querySelector("img").src,
+          "quantity": parseInt(item.querySelector(".details p").innerHTML.slice(10)),
+          "price": parseFloat(item.querySelector('p:nth-child(2)').innerHTML.slice(8))
+        }
+      })
+    }
+    return data;
+  })
+  browser.close() //close browser window/end session
+  jsonfile.writeFile("data.json", list) //export to/create and export 'list'
 }
 
 obtainData('https://dev-test.hudsonstaging.co.uk/')
